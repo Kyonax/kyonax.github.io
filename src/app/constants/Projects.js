94571ee74e0419c2eea3cf36ecf524e8d6d699e5
Cristian D. Moreno - Kyonax <kyonax25@gmail.com>
2025-06-06 04:05:36 -0500

    Updating Projects
    Modified-by: Cristian D. Moreno - Kyonax <kyonax25@gmail.com>

--- src/app/constants/Projects.js
+++ src/app/constants/Projects.js
@@ -3,9 +3,9 @@ const PROJECTS = {
     name: `WEDDING INVITE #001`,
     url: `#on-going`,
     deadlines: {
-      "repo creation": `Jun 05 21:00:30 2025`,
-      "web structure": `Jun 07 22:00:03 2025`,
-      "loading page": `Jun 08 22:00:03 2025`,
+      "repo creation": `Jun 06 21:00:30 2025`,
+      "web structure": `Jun 08 19:00:03 2025`,
+      "loading page": `Jun 11 22:00:03 2025`,
     },
   },
   "github-images": {
