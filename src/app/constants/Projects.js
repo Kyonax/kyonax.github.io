748ca410b8ceb09275f2919295fb659fc8660c00
Cristian D. Moreno - Kyonax <iam@kyo.wtf>
2025-06-11 08:26:57 -0500

    Projects Data Update
    Modified-by: Cristian D. Moreno - Kyonax <iam@kyo.wtf>

--- src/app/constants/Projects.js
+++ src/app/constants/Projects.js
@@ -1,11 +1,10 @@
 const PROJECTS = {
   "sofia-married": {
-    name: `WEDDING INVITE #001`,
-    url: `#on-going`,
+    name: `THE INVITE S&C`,
+    url: `https://kyonax.github.io/the-invite-sc/`,
     deadlines: {
-      "repo creation": `Jun 06 21:00:30 2025`,
-      "web structure": `Jun 08 19:00:03 2025`,
       "loading page": `Jun 11 22:00:03 2025`,
+      "first section": `Jun 13 22:00:03 2025`,
     },
   },
   "github-images": {
