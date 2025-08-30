0725e1ca3017e334c111f42b8c184d5f7696738f
Cristian D. Moreno - Kyonax <kyonax25@gmail.com>
2025-08-29 19:24:00 -0500

    Updating CVs and Updating Layout of Projects
    Modified-by: Cristian D. Moreno - Kyonax <kyonax25@gmail.com>

--- src/app/constants/Projects.js
+++ src/app/constants/Projects.js
@@ -6,20 +6,20 @@ const PROJECTS = {
       "refining last changes": `Aug 30 22:00:03 2025`,
     },
   },
-  "veyra-organization": {
-    name: `VEYRA ORG LANDING PAGE`,
-    url: `https://github.com/veyra-code`,
-    deadlines: {
-      "create repo": `Aug 31 22:00:03 2025`,
-    },
-  },
-  "zeronet-labs": {
-    name: `ZERONET LABS LANDING PAGE`,
-    url: `https://github.com/zeronet-labs`,
-    deadlines: {
-      "create repo": `Aug 31 22:00:03 2025`,
-    },
-  },
+  // "veyra-organization": {
+  //   name: `VEYRA ORG LANDING PAGE`,
+  //   url: `https://github.com/veyra-code`,
+  //   deadlines: {
+  //     "create repo": `Aug 31 22:00:03 2025`,
+  //   },
+  // },
+  // "zeronet-labs": {
+  //   name: `ZERONET LABS LANDING PAGE`,
+  //   url: `https://github.com/zeronet-labs`,
+  //   deadlines: {
+  //     "create repo": `Aug 31 22:00:03 2025`,
+  //   },
+  // },
 };
 
 module.exports = { PROJECTS };
